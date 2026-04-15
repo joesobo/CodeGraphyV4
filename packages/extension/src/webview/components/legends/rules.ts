59ec44ab6566276a4593cd69b2c03e5a8ddab78e
joesobo <soboleskijoe@gmail.com>
2026-04-15 09:45:20 -0700

    refactor(webview): split legends panel

---
+++ packages/extension/src/webview/components/legends/rules.ts
@@ -0,0 +1,15 @@
+export {
+  replaceSectionRules,
+  resolveDisplayRules,
+  shouldRenderRuleInSection,
+} from './displayRules';
+export {
+  createLegendRuleId,
+  reorderItems,
+  sendUserLegendRules,
+} from './messages';
+export type {
+  LegendBuiltInEntry,
+  LegendDisplayRule,
+  LegendTargetSection,
+} from './types';
