ac47175bd6c8a2217d6c6bd900cde4a9943aec2d
joesobo <soboleskijoe@gmail.com>
2026-04-15 20:59:19 -0700

    test(extension): cover graph control defaults

--- packages/extension/src/shared/graphControls/defaults/nodeTypes.ts
+++ packages/extension/src/shared/graphControls/defaults/nodeTypes.ts
@@ -5,23 +5,27 @@ import {
   DEFAULT_PACKAGE_NODE_COLOR,
 } from '../../fileColors';
 
-export const CORE_GRAPH_NODE_TYPES: IGraphNodeTypeDefinition[] = [
-  {
-    id: 'file',
-    label: 'Files',
-    defaultColor: DEFAULT_NODE_COLOR,
-    defaultVisible: true,
-  },
-  {
-    id: 'folder',
-    label: 'Folders',
-    defaultColor: DEFAULT_FOLDER_NODE_COLOR,
-    defaultVisible: false,
-  },
-  {
-    id: 'package',
-    label: 'Packages',
-    defaultColor: DEFAULT_PACKAGE_NODE_COLOR,
-    defaultVisible: false,
-  },
-];
+export function createCoreGraphNodeTypes(): IGraphNodeTypeDefinition[] {
+  return [
+    {
+      id: 'file',
+      label: 'Files',
+      defaultColor: DEFAULT_NODE_COLOR,
+      defaultVisible: true,
+    },
+    {
+      id: 'folder',
+      label: 'Folders',
+      defaultColor: DEFAULT_FOLDER_NODE_COLOR,
+      defaultVisible: false,
+    },
+    {
+      id: 'package',
+      label: 'Packages',
+      defaultColor: DEFAULT_PACKAGE_NODE_COLOR,
+      defaultVisible: false,
+    },
+  ];
+}
+
+export const CORE_GRAPH_NODE_TYPES: IGraphNodeTypeDefinition[] = createCoreGraphNodeTypes();
