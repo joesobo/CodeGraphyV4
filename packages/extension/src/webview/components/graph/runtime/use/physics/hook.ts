4ecfbe758ab3b89189bb863319f432f61ad9f9f4
joesobo <soboleskijoe@gmail.com>
2026-04-18 11:08:33 -0700

    Flatten physics hook init modules

--- packages/extension/src/webview/components/graph/runtime/use/physics/hook.ts
+++ packages/extension/src/webview/components/graph/runtime/use/physics/hook.ts
@@ -3,7 +3,7 @@ import type { ForceGraphMethods as FG2DMethods } from 'react-force-graph-2d';
 import type { ForceGraphMethods as FG3DMethods } from 'react-force-graph-3d';
 import type { IPhysicsSettings } from '../../../../../../shared/settings/physics';
 import type { FGLink, FGNode } from '../../../model/build';
-import { usePhysicsRuntimeInit } from './hook/init/effect';
+import { usePhysicsRuntimeInit } from './hook/init';
 import { usePhysicsRuntimeLayoutKey, usePhysicsRuntimeLayoutReset } from './hook/layout';
 import { usePhysicsRuntimePause } from './hook/pause';
 import { usePhysicsRuntimeUpdates } from './hook/updates';
