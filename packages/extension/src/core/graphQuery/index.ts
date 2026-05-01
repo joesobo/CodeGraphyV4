61a9c2273c7c2c642fc6a024a4d017de6d5e9950
joesobo <soboleskijoe@gmail.com>
2026-04-30 17:18:21 -0700

    feat: add core graph query reports

---
+++ packages/extension/src/core/graphQuery/index.ts
@@ -0,0 +1,13 @@
+export type {
+  GraphQueryConfig,
+  GraphQueryEdgeReport,
+  GraphQueryEdgeReportItem,
+  GraphQueryFilter,
+  GraphQueryFilterOperator,
+  GraphQueryNodeReport,
+  GraphQueryNodeReportItem,
+  GraphQueryPage,
+  GraphQueryScope,
+  GraphQuerySort,
+} from './model';
+export { listGraphEdges, listGraphNodes } from './reports';
