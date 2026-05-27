8b1e604854723697e89ce2df208dacaa095e99fe
joesobo <soboleskijoe@gmail.com>
2026-05-27 10:50:37 -0700

    test(extension): split graph appearance model

---
+++ packages/extension/src/webview/components/graph/appearance/contracts.ts
@@ -0,0 +1,13 @@
+export interface GraphAppearance {
+  focusBorder: string;
+  labelForeground: string;
+  labelMutedForeground: string;
+  linkHighlight: string;
+  linkMuted: string;
+  meshDimmed: string;
+  meshSelected: string;
+  nodeSelectionBorder: string;
+  stageBackground: string;
+  stageBorder: string;
+  transparent: string;
+}
