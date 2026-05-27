d049a9405ee234382a78762f7bd1baf6ea6605e8
joesobo <soboleskijoe@gmail.com>
2026-05-26 18:39:06 -0700

    test(extension): split delete files action

---
+++ packages/extension/src/extension/actions/deleteFiles/backup.ts
@@ -0,0 +1,53 @@
+import * as vscode from 'vscode';
+
+export interface StoredFile {
+  path: string;
+  content: Uint8Array;
+}
+
+function isDirectoryStat(stat: vscode.FileStat): boolean {
+  return (stat.type & vscode.FileType.Directory) !== 0;
+}
+
+export async function storeDirectoryForDeletion(
+  directoryPath: string,
+  directoryUri: vscode.Uri,
+  storedDirectories: string[],
+  storedFiles: StoredFile[],
+): Promise<void> {
+  storedDirectories.push(directoryPath);
+
+  const entries = await vscode.workspace.fs.readDirectory(directoryUri);
+  for (const [entryName, entryType] of entries) {
+    const entryPath = `${directoryPath}/${entryName}`;
+    const entryUri = vscode.Uri.joinPath(directoryUri, entryName);
+
+    if ((entryType & vscode.FileType.Directory) !== 0) {
+      await storeDirectoryForDeletion(entryPath, entryUri, storedDirectories, storedFiles);
+      continue;
+    }
+
+    const content = await vscode.workspace.fs.readFile(entryUri);
+    storedFiles.push({ path: entryPath, content });
+  }
+}
+
+export async function storePathForDeletion(
+  workspaceFolder: vscode.Uri,
+  filePath: string,
+  storedDirectories: string[],
+  storedFiles: StoredFile[],
+): Promise<void> {
+  const fileUri = vscode.Uri.joinPath(workspaceFolder, filePath);
+  const stat = await vscode.workspace.fs.stat(fileUri);
+
+  if (isDirectoryStat(stat)) {
+    await storeDirectoryForDeletion(filePath, fileUri, storedDirectories, storedFiles);
+    await vscode.workspace.fs.delete(fileUri, { recursive: true, useTrash: true });
+    return;
+  }
+
+  const content = await vscode.workspace.fs.readFile(fileUri);
+  storedFiles.push({ path: filePath, content });
+  await vscode.workspace.fs.delete(fileUri, { useTrash: true });
+}
