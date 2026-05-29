bdd203dee1c710b36fa15a42420787d130e01ef0
joesobo <soboleskijoe@gmail.com>
2026-05-29 12:27:00 -0700

    refactor(core): own plugin status queries

--- packages/extension/src/extension/pipeline/plugins/queries.ts
+++ packages/extension/src/extension/pipeline/plugins/queries.ts
@@ -1,59 +1,6 @@
-import * as path from 'path';
-import type { CodeGraphyInstalledPluginRecord, IDiscoveredFile } from '@codegraphy-dev/core';
-import type { IProjectedConnection, IPlugin } from '../../../core/plugins/types/contracts';
-import type { PluginRegistry } from '../../../core/plugins/registry/manager';
-import type { IPluginStatus } from '../../../shared/plugins/status';
-import { buildWorkspacePluginStatuses } from './statusBuilder';
-
-interface WorkspacePipelinePluginRegistry {
-  getPluginForFile(filePath: string): IPlugin | undefined;
-  list(): ReturnType<PluginRegistry['list']>;
-}
-
-export interface WorkspacePipelinePluginStatusDependencies {
-  disabledPlugins: ReadonlySet<string>;
-  discoveredFiles: IDiscoveredFile[];
-  fileConnections: Map<string, IProjectedConnection[]>;
-  installedPlugins?: readonly CodeGraphyInstalledPluginRecord[];
-  registry: WorkspacePipelinePluginRegistry;
-  workspaceEnabledPackageNames?: ReadonlySet<string>;
-}
-
-export function getWorkspacePipelinePluginStatuses(
-  dependencies: WorkspacePipelinePluginStatusDependencies,
-): IPluginStatus[] {
-  return buildWorkspacePluginStatuses({
-    disabledPlugins: dependencies.disabledPlugins,
-    discoveredFiles: dependencies.discoveredFiles,
-    fileConnections: dependencies.fileConnections,
-    installedPlugins: dependencies.installedPlugins,
-    pluginInfos: dependencies.registry.list(),
-    workspaceEnabledPackageNames: dependencies.workspaceEnabledPackageNames,
-  });
-}
-
-export function getWorkspacePipelinePluginNameForFile(
-  relativePath: string,
-  workspaceRoot: string,
-  registry: Pick<WorkspacePipelinePluginRegistry, 'getPluginForFile'>,
-): string | undefined {
-  return registry.getPluginForFile(path.join(workspaceRoot, relativePath))?.name;
-}
-
-export function resolveWorkspacePipelinePluginNameForFile(
-  relativePath: string,
-  lastWorkspaceRoot: string,
-  getWorkspaceRoot: () => string | undefined,
-  registry: Pick<WorkspacePipelinePluginRegistry, 'getPluginForFile'>,
-): string | undefined {
-  const workspaceRoot = lastWorkspaceRoot || getWorkspaceRoot();
-  if (!workspaceRoot) {
-    return undefined;
-  }
-
-  return getWorkspacePipelinePluginNameForFile(
-    relativePath,
-    workspaceRoot,
-    registry,
-  );
-}
+export {
+  getWorkspaceIndexPluginNameForFile as getWorkspacePipelinePluginNameForFile,
+  getWorkspaceIndexPluginStatuses as getWorkspacePipelinePluginStatuses,
+  resolveWorkspaceIndexPluginNameForFile as resolveWorkspacePipelinePluginNameForFile,
+  type WorkspaceIndexPluginStatusDependencies as WorkspacePipelinePluginStatusDependencies,
+} from '@codegraphy-dev/core';
