ff607cbb398c1bca5b6c0528306572cf859f2d92
joesobo <soboleskijoe@gmail.com>
2026-05-29 17:13:26 -0700

    Show TypeScript alias imports in example

--- examples/example-typescript/src/index.ts
+++ examples/example-typescript/src/index.ts
@@ -1,3 +1,4 @@
+import { buildAliasGreeting } from '@example/greeting';
 import { formatUser } from './types';
 import type { UserName } from './types';
 import { buildGreeting } from './utils';
@@ -5,3 +6,4 @@ import { buildGreeting } from './utils';
 export const currentUser: UserName = formatUser('CodeGraphy');
 
 console.log(buildGreeting(currentUser));
+console.log(buildAliasGreeting(currentUser));
